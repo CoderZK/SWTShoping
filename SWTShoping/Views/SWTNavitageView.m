10c53a0afeec54d0c6b9a8210c0b39bee0d7c48b
kunzhang <542283618@qq.com>
2020-07-04 16:07:40 +0800

    uuuu

---
+++ SWTShoping/Views/SWTNavitageView.m
@@ -0,0 +1,21 @@
+//
+//  SWTNavitageView.m
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/7/4.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "SWTNavitageView.h"
+
+@implementation SWTNavitageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ SWTShoping/首页/Views/SWTGoodsDetailHeadV.m
@@ -0,0 +1,21 @@
+//
+//  SWTGoodsDetailHeadV.m
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/7/4.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "SWTGoodsDetailHeadV.h"
+
+@implementation SWTGoodsDetailHeadV
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
