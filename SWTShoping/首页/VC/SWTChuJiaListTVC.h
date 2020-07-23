1e22606f83f60958f25ed14facea391a2a03af26
kunzhang <542283618@qq.com>
2020-07-23 17:53:06 +0800

    no message

---
+++ SWTShoping/首页/VC/SWTChuJiaListTVC.h
@@ -0,0 +1,17 @@
+//
+//  SWTChuJiaListTVC.h
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/7/23.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SWTChuJiaListTVC : BaseTableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
