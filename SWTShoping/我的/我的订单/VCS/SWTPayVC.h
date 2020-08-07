1d1f1f883a8876a51fa8b67b19ef9b4422704f07
kunzhang <542283618@qq.com>
2020-08-07 22:50:39 +0800

    yyyy

---
+++ SWTShoping/我的/我的订单/VCS/SWTPayVC.h
@@ -0,0 +1,17 @@
+//
+//  SWTPayVC.h
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/8/7.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SWTPayVC : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SWTShoping/我的/我的订单/VCS/SWTPayVC.m
@@ -0,0 +1,32 @@
+//
+//  SWTPayVC.m
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/8/7.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "SWTPayVC.h"
+
+@interface SWTPayVC ()
+
+@end
+
+@implementation SWTPayVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
