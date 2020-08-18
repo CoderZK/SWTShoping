17ca0e2d4322c3354f617f035d1e1acb0df7fdad
kunzhang <542283618@qq.com>
2020-08-18 23:01:16 +0800

    yyy

--- SWTShoping/SWTURLDefineTool.h
+++ SWTShoping/SWTURLDefineTool.h
@@ -15,9 +15,9 @@
 //#define URLOne @"http://192.168.0.38/jkgl/"
 #define URLOne @"http://139.129.47.30:8080/admin/"
 
-////图片地址
+//图片地址
 #define QiNiuImgURL @"http://web.qunyanzhujia.com/"
-////视频地址地址
+//视频地址地址
 #define QiNiuVideoURL @"http://media.qunyanzhujia.com/"
 //骑牛云的上传地址
 #define QiNiuYunUploadURL @"http://upload.qiniup.com/"
@@ -49,10 +49,7 @@
 #pragma mark ---- 商户信息接口  -----
 /** 加入黑名单 */
 #define merchAdd_black_merch_SWT URLOne@"merch/add_black_merch"
-/** 获取经营主体 */
-#define merchBusinessentity_SWT URLOne@"merch/businessentity"
-/**获取所有顶级分类 */
-#define merchCategory_SWT URLOne@"merch/category"
+
 /** 获取商户信息 */
 #define merchDetail_SWT URLOne@"merch/detail"
 /**
@@ -81,8 +78,9 @@
 /** 获取商品的所有顶级分类 */
 #define goodTopcategory_SWT URLOne@"good/topcategory"
 
-/** 注册第一步*/
-#define registerStep1_SWT URLOne@"register/step1"
+#pragma mark ---- 商户信息接口  -----
+
+
 
 /**注册第二部*/
 #define registerStep2_SWT URLOne@"register/step2"
@@ -91,14 +89,14 @@
 
 
 #pragma mark ---- 帮助信息模   -----
-///** 提交帮助 */
+/** 提交帮助 */
 #define helpCommit_SWT URLOne@"help/commit"
 
-///** 帮助模块 */
+/** 帮助模块 */
 #define helpTop_SWT URLOne@"help/top"
-///** 帮助模块的子列表 */
+/** 帮助模块的子列表 */
 #define helpList_SWT URLOne@"help/list"
-///**帮助模块详情 */
+/**帮助模块详情 */
 #define helpDetail_SWT URLOne@"help/detail"
 
 
@@ -124,23 +122,23 @@
 
 /** 获取我的优惠券列表 */
 #define userCoupon_SWT URLOne@"user/coupon"
-///** 获取用信息 */
+/** 获取用信息 */
 #define userDetail_SWT URLOne@"user/detail"
 /** 修改密码 */
 #define editpassword_SWT URLOne@"user/editpassword"
-///** 获取我的收藏 */
+/** 获取我的收藏 */
 #define userFav_SWT URLOne@"user/fav"
-///** 收藏 */
+/** 收藏 */
 #define userFavOperate_SWT URLOne@"user/fav/operate"
 
-///** 用户关注的 */
+/** 用户关注的 */
 #define userFollow_SWT URLOne@"user/follow"
-///** 关注操作 */
+/** 关注操作 */
 #define userFollowOperate_SWT URLOne@"user/follow/operate"
 
-///** 猜你喜欢 */
+/** 猜你喜欢 */
 #define userLike_SWT URLOne@"user/like"
-///** 我的钱包 */
+/** 我的钱包 */
 #define userMypackage_SWT URLOne@"user/mypackage"
 
 /**设置支付密码接口 */
@@ -187,11 +185,11 @@
 
 #pragma mark ---- 视频信息接口 -----
 
-///**获取顶部视频信息 */
+/**获取顶部视频信息 */
 #define videoTop_SWT URLOne@"video/top"
-///** 获取视频详情 */
+/** 获取视频详情 */
 #define videoDetail_SWT URLOne@"video/detail"
-///** 根据视频分类获取视频列表 */
+/** 根据视频分类获取视频列表 */
 #define videoList_SWT URLOne@"video/list"
 /** 视频分裂接口 */
 #define videoCategory_SWT URLOne@"video/category"
@@ -290,7 +288,7 @@
 #define lideshow_SWT URLOne@"index/lideshow"
 /** 获取今日热门 */
 #define indexHot_SWT URLOne@"index/hot"
-///** 获取用户协议 */
+/** 获取用户协议 */
 #define indexRecommend_SWT URLOne@"index/recommend"
 
 
@@ -299,16 +297,139 @@
 #define payOrder_SWT URLOne@"pay/order"
 /** 账单查询 */
 #define payOrderQuery_SWT URLOne@"pay/orderQuery"
-///**  */
+/**  */
 #define payOrdersuccess_SWT URLOne@"pay/ordersuccess"
 
-///** 订单查询支付状态 */
+/** 订单查询支付状态 */
 #define payOrderquery_SWT URLOne@"pay/orderquery"
 
 
+#pragma mark ---- 商户端注册模块 -----
+
+/** 获取注册页面展示信息配置*/
+#define registerGet_info_SWT URLOne@"register/get_info"
+
+/** 获取经营主体 */
+#define merchBusinessentity_SWT URLOne@"merch/businessentity"
+/**获取所有顶级分类 */
+#define merchCategory_SWT URLOne@"merch/category"
+/** 获取商户主营类目*/
+#define merchSellcate_SWT URLOne@"merch/sellcate"
+
+#pragma mark ---- 商户端信息  -----
+/** 获取商户信息*/
+#define merchGet_merchinfo_SWT URLOne@"merch/get_merchinfo"
+/** 商户设置*/
+#define merchUpd_merchinfo_SWT URLOne@"merch/upd_merchinfo"
+/** 检查是否开启商品库 */
+#define merchCheck_open_SWT URLOne@"merch/check_open"
+/** 开启商品库 */
+#define merchDo_open_SWT URLOne@"merch/do_open"
+/** 直播列表 */
+#define merchGet_live_list_SWT URLOne@"merch/get_live_list"
+/** 直播订单 */
+#define merchGet_live_order_list_SWT URLOne@"merch/get_live_order_list"
+/** 商品库页面信息 */
+#define merchStatic_warehouse_SWT URLOne@"merch/static_warehouse"
+
+#pragma mark ----- 商户端订单 -----
+
+/** 获取商户订单*/
+#define merchorderGet_order_list_merch_SWT URLOne@"merchorder/get_order_list_merch"
+/** 获取子订单 */
+#define merchorderGet_children_order_list_SWT URLOne@"merchorder/get_children_order_list"
+/** 发货 */
+#define merchorderSend_order_merch_SWT URLOne@"merchorder/send_order_merch"
+/** 售后信息 */
+#define merchorderGet_refund_info_SWT URLOne@"merchorder/get_refund_info"
+/** 售后处理 */
+#define merchorderAudit_order_merch_SWT URLOne@"merchorder/audit_order_merch"
+
+#pragma mark ----- 商户端拍品 -----
+
+/** 拍品列表*/
+#define merchauctionGet_goods_list_SWT URLOne@"merchauction/get_goods_list"
+/** 上下架拍品*/
+#define merchauctionPull_goods_SWT URLOne@"merchauction/pull_goods"
+/**统计竞拍中商品 */
+#define merchauctionCount_auction_goods_SWT URLOne@"merchauction/count_auction_goods"
+
+#pragma mark ----- 商户端商品 -----
+/** 获取商品库信息 */
+#define merchgoodsGet_warehouse_info_SWT URLOne@"merchgoods/get_warehouse_info"
+/** 获取商品分类 */
+#define erchgoodsGet_goods_cate_SWT URLOne@"erchgoods/get_goods_cate"
+/** 获取商品库 */
+#define merchgoodsGet_warehouse_list_SWT URLOne@"merchgoods/get_warehouse_list"
+/** 获取商品材质*/
+#define merchgoodsGet_material_list_SWT URLOne@"merchgoods/get_material_list"
+/** 添加商品*/
+#define merchgoodsAdd_goods_SWT URLOne@"merchgoods/add_goods"
+/** 修改商品 */
+#define merchgoodsUpd_goods_SWT URLOne@"merchgoods/upd_goods"
+/** 获取商品列表 */
+#define merchgoodsGet_goods_list_merch_SWT URLOne@"merchgoods/get_goods_list_merch"
+/** 删除商品*/
+#define merchgoodsDel_goods_SWT URLOne@"merchgoods/del_goods"
+/** 搜索商品*/
+#define merchgoodsSer_goods_SWT URLOne@"merchgoods/ser_goods"
+/** 获取商品详情*/
+#define merchgoodsGet_goods_detail_SWT URLOne@"merchgoods/get_goods_detail"
+
+#pragma mark ----- 商户端优惠券 -----
+/**添加优惠券 */
+#define mmerchcouponAdd_coupon_SWT URLOne@"merchcoupon/add_coupon"
+/** 获取优惠券列表 */
+#define merchcouponGet_coupon_list_SWT URLOne@"merchcoupon/get_coupon_list"
 
 
 
+/**获取商家接收消息*/
+#define merchmsgGet_msg_list_SWT URLOne@"merchmsg/get_msg_list"
+/** 获取优惠券列表 */
+#define merchcouponGet_coupon_list_SWT URLOne@"merchcoupon/get_coupon_list"
+
+#pragma mark ----- 商户端报表 -----
+/**资金报表 */
+#define merchreportGet_finance_report_SWT URLOne@"merchreport/get_finance_report"
+/** 成拍列表*/
+#define merchreportGet_ok_list_SWT URLOne@"merchreport/get_ok_list"
+/**支付列表 */
+#define merchreportGet_pay_list_SWT URLOne@"merchreport/get_pay_list"
+/** 退款列表*/
+#define merchreportGet_refund_list_SWT URLOne@"merchreport/get_refund_list"
+/**收款列表*/
+#define merchreportGet_earn_list_SWT URLOne@"merchreport/get_earn_list"
+/** 店铺报表*/
+#define merchreportGet_merch_report_SWT URLOne@"merchreport/get_merch_report"
+/** 直播订单列表*/
+#define merchGet_ok_list_SWT URLOne@"merch/get_ok_list"
+
+#pragma mark ----- 商户端合买 -----
+/**合买抽签*/
+#define merchlotsDraw_lots_SWT URLOne@"merchlots/draw_lots"
+/** 获取抽签订单信息*/
+#define merchlotsGet_lots_result_SWT URLOne@"merchlots/get_lots_result"
+/**发料*/
+#define merchlotsSend_lots_SWT URLOne@"merchlots/send_lots"
+/** 获取合买总订单*/
+#define merchcouponGet_coupon_list_SWT URLOne@"merchcoupon/get_coupon_list"
+/**获取合买分订单*/
+#define merchlotsGet_main_order_SWT URLOne@"merchlots/get_main_order"
+/** 获取合买分订单*/
+#define merchlotsGet_chipped_order_SWT URLOne@"merchlots/get_chipped_order"
+/**合买商品资料上传*/
+#define merchlotsAdd_lots_info_SWT URLOne@"merchlots/add_lots_info"
+/** 获取上传资料记录*/
+#define merchlotsGet_lots_info_SWT URLOne@"merchlots/get_lots_info"
+///**合买抽签*/
+//#define mmerchcouponAdd_coupon_SWT URLOne@"merchcoupon/add_coupon"
+///** 获取抽签订单信息*/
+//#define merchcouponGet_coupon_list_SWT URLOne@"merchcoupon/get_coupon_list"
+///**合买抽签*/
+//#define mmerchcouponAdd_coupon_SWT URLOne@"merchcoupon/add_coupon"
+///** 获取抽签订单信息*/
+//#define merchcouponGet_coupon_list_SWT URLOne@"merchcoupon/get_coupon_list"
 
 
 #endif /* SWTURLDefineTool_h */
