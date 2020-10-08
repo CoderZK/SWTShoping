e1cb4dcede8f7a99cf9747eff25832a82e28663f
kunzhang <542283618@qq.com>
2020-10-08 22:57:50 +0800

    no message

---
+++ SWTShoping/直播/views/SWTZhiBoFootView.m
@@ -0,0 +1,21 @@
+//
+//  SWTZhiBoFootView.m
+//  SWTShoping
+//
+//  Created by kunzhang on 2020/10/8.
+//  Copyright © 2020 kunzhang. All rights reserved.
+//
+
+#import "SWTZhiBoFootView.h"
+
+@implementation SWTZhiBoFootView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
